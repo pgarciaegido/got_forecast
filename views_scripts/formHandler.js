56f1c27b5a20db435ba1b1f8c5c595d6b9e67f68
Pablo García Egido <pablo@caseonit.com>
2019-02-13 22:27:36 +0100

    sending form properly to backend

--- views_scripts/formHandler.js
+++ views_scripts/formHandler.js
@@ -1,8 +1,51 @@
 const formSerialize = require('form-serialize');
 
-const storeAnswers = (form) => {
+const localStorageName = 'gotForecast';
+
+const storeAnswers = (form, pageTo) => {
   var obj = formSerialize(form, { hash: true });
-  console.log(obj);
+
+  // pageTo 2 means that we are currently on page 1.
+  pageTo === 2 ? setLocalStorageItem(obj) : setLocalStorageItem(Object.assign(getLocalStorageItem(), obj));
+
+  redirectToNextPage(pageTo)
 }
 
-module.exports = { storeAnswers };
+const submitForm = (form) => {
+  var obj = formSerialize(form, { hash: true });
+  const formAnswers = Object.assign(getLocalStorageItem() , obj);
+
+  var newForm = createForm();
+
+  Object.keys(formAnswers).forEach((key) => newForm.appendChild(createInput(formAnswers, key)));
+
+  document.body.appendChild(newForm);
+  
+  newForm.submit();
+};
+
+const getLocalStorageItem = () => JSON.parse(localStorage.getItem(localStorageName));
+
+const setLocalStorageItem = (obj) => localStorage.setItem(localStorageName, JSON.stringify(obj));
+
+const redirectToNextPage = (pageTo) => window.location.href = `/quest?p=${pageTo}`;
+
+const createForm = () => {
+  const form = document.createElement("form");
+
+  form.method = "POST";
+  form.action = "/submit"
+  return form;
+};
+
+const createInput = (formAnswers, key) => {
+  const input = document.createElement("input");
+  input.type = "radio";
+  input.name = key;
+  input.value = formAnswers[key];
+  input.checked = "checked";
+  input.style.display = 'none';
+  return input;
+};
+
+module.exports = { storeAnswers, submitForm };
