9945056251a917c6ef1597c6f4ba0b80f732e963
Pablo Egido <pgarciaegido@gmail.com>
2019-02-22 20:56:54 +0100

    styling character card

--- views_scripts/index.js
+++ views_scripts/index.js
@@ -1,9 +1,7 @@
-const pictureHandler = require('./pictureHandler');
 const formHandler = require('./formHandler');
 const download = require('./download');
 
 module.exports = {
   download,
-  pictureHandler,
   formHandler
 }
