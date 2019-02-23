be327d818ed90da920cd30abc3a0e439152d0498
Pablo Egido <pgarciaegido@gmail.com>
2019-02-23 12:55:20 +0100

    responsive form card

--- bin/deploy-heroku.sh
+++ bin/deploy-heroku.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
+
+commitSHA=$(git rev-parse --short HEAD)
+commitMessage=$(git log -1 --pretty=%B)
+
+rm -rf public
 git checkout master
 git merge develop --no-edit
 npm run build
 git add .
-git commit -m "build public folder"
+git commit -m "Build public folder. Latest commit: $commitMessage ($commitSHA)"
 git push heroku master
 git checkout develop
