ae47ee7ca1d47d326e834d3a087a8616bd5622e4
Pablo Egido <pgarciaegido@gmail.com>
2019-03-16 20:11:44 +0100

    test

--- bin/deploy-heroku.sh
+++ bin/deploy-heroku.sh
@@ -7,13 +7,20 @@ npm test
 
 commitSHA=$(git rev-parse --short HEAD)
 commitMessage=$(git log -1 --pretty=%B)
+random=$RANDOM
 
 rm -rf public
 git checkout master
 rm -rf public
 git merge develop --no-edit
 npm run build
+cd public
+touch $random
+cd ..
 git add .
 git commit -m "Build public folder. Latest commit: $commitMessage ($commitSHA)"
 git push heroku master
+cd public
+rm $random
+cd ..
 git checkout develop
